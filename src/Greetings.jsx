d04465324850b2e36fd242e5d3057c84c1fa48a5
Plia Vang <pliaannevang.02@gmail.com>
2024-02-02 11:52:16 +0100

    Condition the display (conditional rendering)

--- src/Greetings.jsx
+++ src/Greetings.jsx
@@ -1,12 +1,20 @@
 export function Greetings(props) {
   console.log('***', props);
-  return (
-    <ul>
-      <li>{props.nameVegetable}</li>
-      <li>Le nombres de carotte en stock: {props.quantity}</li>
 
-      {/* Second way to do it */}
-      <li>{props.children}</li>
-    </ul>
-  );
+  // conditional rendering --> We can condition the display
+  if (props.quantity < 100) {
+    return (
+      <p>Plus de stock</p>
+    );
+  } else {
+    return (
+      <><p>Le nombres à acheter n'est pas limiter</p><ul>
+        <li>{props.nameVegetable}</li>
+        <li>Le nombres de carotte en stock: {props.quantity}</li>
+
+        {/* Second way to do it */}
+        <li>{props.children}</li>
+      </ul></>
+    );
+  }
 }
