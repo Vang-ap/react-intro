b2aafebdc7c30a41e369fe6ef540574d75a7c74e
Plia Vang <pliaannevang.02@gmail.com>
2024-02-05 16:49:38 +0100

    Added +1 to age with each click. Each change of state allows a re-rendering

--- src/AgeCount.jsx
+++ src/AgeCount.jsx
@@ -1,11 +1,18 @@
+import { useState } from 'react';
+
 export function AgeCounter(props) {
-  let age = 30;
+  const [age, setAge] = useState(30);
+  // const [age, setAge] = useState(30);
+  // age = contains the value
+  // setAge = function to change the value - sets the value
+  // useState(30) = initial value
   
   function increaseAge() {
     // Adding +1 to age every time we click
-    age = age + 1;
+    setAge(age + 1);
   }
   
+  console.log("*** rerender");
   return (
     <>
         <div>
