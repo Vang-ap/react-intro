7e9809d76b134bb47e0c7cbc55909489d47b0bd7
Plia Vang <pliaannevang.02@gmail.com>
2024-02-12 11:00:07 +0100

    Refacto "menuList" and used map()

--- src/components/MenuList/MenuList.jsx
+++ src/components/MenuList/MenuList.jsx
@@ -1,29 +1,19 @@
 import { MenuListItem } from "../MenuListItem/MenuListItem";
+import { DIFFICULTY_LEVELS } from "./constant";
 import styleMenuList from "./style.module.css";
 
 export function MenuList({ difficulty, onItemClick }) {
   return (
     <div className={styleMenuList.container}>
-      <MenuListItem
-        onClick={onItemClick}
-        difficulty="Low"
-        isSelected={difficulty === "Low"}
-      />
-      <MenuListItem
-        onClick={onItemClick}
-        difficulty="Medium"
-        isSelected={difficulty === "Medium"}
-      />
-      <MenuListItem
-        onClick={onItemClick}
-        difficulty="Hight"
-        isSelected={difficulty === "Hight"}
-      />
-      <MenuListItem
-        onClick={onItemClick}
-        difficulty="Insane"
-        isSelected={difficulty === "Insane"}
-      />
+      {DIFFICULTY_LEVELS.map((diff) => {
+        return (
+          <MenuListItem
+            onClick={onItemClick}
+            difficulty={diff}
+            isSelected={difficulty === diff}
+          />
+        );
+      })}
     </div>
   );
 }
---
+++ src/components/MenuList/constant.js
@@ -0,0 +1,1 @@
+export const DIFFICULTY_LEVELS = ["Low", "Medium", "Hight", "Insane"];
